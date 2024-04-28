bb678dcf7746aa3346fb7c143d32e843f2a5b9b5
Namil27 <namil05@yandex.ru>
2024-04-28 22:00:47 +0300

    [REWORK] reworked and optimizated

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
-import datetime
+from datetime import datetime
+
+import psycopg2
+
+from DataBase_connection import *
 from full_cycle_funcs import *
 
 
@@ -6,13 +10,23 @@ def full_cycle():
     """
     Функция собирающая данные и записывающая их в БД.
     """
-    date = datetime.datetime.today().strftime("%Y-%m-%d")
+    # Подключаемся к базе данных
+    connection = psycopg2.connect(
+        dbname=dbname,
+        user=user,
+        password=password,
+        host=host,
+        port=port
+    )
     # Сохраняем данные в today
     today_media_reit = pars_reit_today(1, 4)
+    date = datetime.now().strftime('%Y-%m-%d')
 
     for media in today_media_reit:
-        add_redaction_table(media[0])
-        add_data_in_table(media[0], date, media[1])
+        add_redaction_table(media[0], connection)
+        add_data_in_table(media[0], date, media[1], connection)
+
+    connection.close()
 
 
 full_cycle()

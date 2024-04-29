6b58d4b2aeecb0b89751992e714c57e0d4796a5e
Namil27 <namil05@yandex.ru>
2024-04-29 22:29:06 +0300

    [ADD] add connection information

---
+++ src/conn_info/con_info.py
@@ -0,0 +1,15 @@
+connect_args_user = {
+    "dbname": "liveinternet",
+    "user": "awesome_user",
+    "password": "russia1917",
+    "host": "23.111.123.4",
+    "port": "5432"
+    }
+
+connect_args_parser = {
+    "dbname": "liveinternet",
+    "user": "namil27",
+    "password": "BigBoy2005",
+    "host": "23.111.123.4",
+    "port": "5432"
+    }

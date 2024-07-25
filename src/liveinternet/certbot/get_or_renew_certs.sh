35059dcd958eac0951e3fab52fc8375c723fbc2e
Namil27 <namil05@yandex.ru>
2024-07-25 13:12:43 +0300

    [FIX] fix nginx

--- src/liveinternet/certbot/get_or_renew_certs.sh
+++ src/liveinternet/certbot/get_or_renew_certs.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 
 # Переменные для домена и email
 DOMAIN=stat.miliutin.ru
 EMAIL=namil05@yandex.ru
+WEBROOT=/var/www/certbot
+
+# Создаем директорию для webroot, если ее нет
+mkdir -p $WEBROOT
 
 # Запуск certbot для получения или обновления сертификатов
-certbot certonly --standalone --non-interactive --agree-tos --email $EMAIL -d $DOMAIN --keep-until-expiring --renew-with-new-domains
-
-# Если сертификаты успешно получены или обновлены, они будут находиться в /etc/letsencrypt/live/$DOMAIN/
-# Добавьте здесь любые дополнительные команды, если необходимо
+certbot certonly --webroot -w $WEBROOT --non-interactive --agree-tos --email $EMAIL -d $DOMAIN --keep-until-expiring --renew-with-new-domains

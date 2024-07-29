fab40e4bd90a2c22529645406ab468cdd911cc38
Namil27 <namil05@yandex.ru>
2024-07-29 15:46:15 +0300

    [FIX] fix nginx

--- src/liveinternet/certbot/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# Получаем сертификат
-certbot certonly --webroot -w /var/www/certbot -d stat.miliutin.ru --non-interactive --agree-tos --email namil05@yandex.ru
-
-# Дополнительные команды, если нужно

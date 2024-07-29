c772b62997cce4560ae97a4165847e243abd6c54
Namil27 <namil05@yandex.ru>
2024-07-29 18:10:48 +0300

    [FIX] fix nginx

--- src/liveinternet/certbot/renew_certificates.sh
+++ src/liveinternet/certbot/renew_certificates.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-certbot renew --quiet && echo "Все отлично, ключи обновились!"
+
+# Запрашиваем сертификаты при запуске контейнера
+certbot certonly --webroot -w /var/www/certbot -d stat.miliutin.ru --non-interactive --agree-tos --email namil05@yandex.ru
+
+# Бесконечный цикл для обновления сертификатов каждую неделю
+while true; do
+  sleep 604800  # 604800 секунд = 1 неделя
+  certbot renew --webroot -w /var/www/certbot
+done
